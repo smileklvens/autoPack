765d264b3e755a1bbb5e386e0c0e5969400adcbc
leike.zhu <leike.zhu@ikang.com>
2019-11-10 13:57:33 +0800

    完善打包,发出错误提醒

--- config.py
+++ config.py
@@ -3,8 +3,8 @@
 #需正确填写app对应的keystore信息
 keystorePath = "/Users/zhulk/workspace/androidproject/android-tijianbao/sign/leaf.keystore"
 keyAlias = "android.keystore"
-keystorePassword = "123456"
-keyPassword = "123456"
+keystorePassword = "XXXXXX"
+keyPassword = "XXXXXX"
 
 #加固后但未使用360签名的源apk（即只使用360软件加固，但不使用360软件签名的apk）
 protectedSourceApkName = "tjbapp_3.10.6_jiagu.apk"
