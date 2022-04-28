4b07f98206ef798de84357db34d70fbb90cfe7b1
zlk <smileklvens@163.com>
2022-04-28 11:32:28 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #coding=utf-8
 
 #需正确填写app对应的keystore信息
-keystorePath = "/Users/zhulk/workspace/androidproject/android-tijianbao/sign/leaf.keystore"
+keystorePath = "你的.keystore"
 keyAlias = "android.keystore"
 keystorePassword = "XXXXXX"
 keyPassword = "XXXXXX"
 
 #加固后但未使用360签名的源apk（即只使用360软件加固，但不使用360软件签名的apk）
-protectedSourceApkName = "tjbapp_3.10.6_jiagu.apk"
+protectedSourceApkName = ".apk"
 #需正确填写你安装的 Android SDK buidtools path please use above 25.0+
 sdkBuildToolPath = "/Users/zhulk/Library/Android/sdk/build-tools/28.0.3"
